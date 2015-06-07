b601d8010a46a44831e6fb2280bcec33a56c9b29
Renan Siravegna <renan.siravegna@gmail.com>
2015-06-07 15:34:42 -0400

    Breve refactor no juiz

--- model/juiz.js
+++ model/juiz.js
@@ -1,33 +1,17 @@
 var jogadaFactory = require("../model/jogadaFactory");
 
 var juiz = {};
-var resultadoDaJogada = { 
-	pedra: "pedra",
-	papel: "papel",
-	tesoura: "tesoura",
-	empate: "empate"
-};
 
 juiz.analisar = function(jogada1, jogada2) {
-	if (jogada1 === "pedra" && jogada2 === "papel")
-		return resultadoDaJogada.papel;
+	var jogada = jogadaFactory.criarPara(jogada1);
 
-	else if (jogada1 === "pedra" && jogada2 === "tesoura")
-		return resultadoDaJogada.pedra;
+	if (jogada.empataCom === jogada2)
+		return "empate";
 
-	else if (jogada1 === "papel" && jogada2 === "pedra")
-		return resultadoDaJogada.papel;
-
-	else if (jogada1 === "papel" && jogada2 === "tesoura")
-		return resultadoDaJogada.tesoura;
-
-	else if (jogada1 === "tesoura" && jogada2 === "pedra")
-		return resultadoDaJogada.pedra;
-
-	else if (jogada1 === "tesoura" && jogada2 === "papel")
-		return resultadoDaJogada.tesoura;
-
-	return resultadoDaJogada.empate;
+	if (jogada.ganhaDe === jogada2)
+		return jogada1;
+	else
+		return jogada2;
 };
 
 module.exports = juiz;
